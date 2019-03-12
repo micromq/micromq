092d7b354e1f215cbf7f1431d1116d80ed27ceb0
bifot <bifot@bifot.ru>
2019-03-12 14:36:52 +0300

    refactored examples/gateway

--- examples/gateway.js
+++ examples/gateway.js
@@ -18,15 +18,8 @@ app.action('authorize', (meta) => {
   return { isAuthorized: true };
 });
 
-app.use((req, res) => {
-  if (!req.url.startsWith('/users')) {
-    res.writeHead(404);
-    res.end('Not Found');
-
-    return;
-  }
-
-  res.delegate('users');
+app.all('/users/(.*)', async (req, res) => {
+  await res.delegate('users');
 });
 
 app.listen(process.env.PORT);
